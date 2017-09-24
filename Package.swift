7ec53d737ca1a91779db84bc68f11faf44b20e78
Marko Justinek <marko.justinek@gmail.com>
2017-09-25 09:08:48 +1000

    Adds dependencies to Package.swift manifest file

--- Package.swift
+++ Package.swift
@@ -4,16 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "PactSwiftPMExample",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "PactSwiftPMExample",
-            dependencies: []),
-    ]
+  name: "PactSwiftPMExample",
+  dependencies: [
+      // Dependencies declare other packages that this package depends on.
+      // .package(url: /* package url */, from: "1.0.0"),
+      .package(url: "https://github.com/surpher/PactConsumer", from: "0.0.2")
+  ],
+  targets: [
+      // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+      // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+      .target(
+          name: "PactSwiftPMExample",
+          dependencies: []),
+      .target(
+          name: "PactSwiftPMExampleTests",
+          dependencies: ["PactConsumer"]),
+  ]
 )
