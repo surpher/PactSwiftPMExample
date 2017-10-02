72c7bd2e6aedc1221ec10344bfd9a8b31d3d0183
Marko Justinek <marko.justinek@gmail.com>
2017-10-03 08:56:01 +1100

    Use build_test.sh in TravisCI

--- build_test.sh
+++ build_test.sh
@@ -1,7 +1,6 @@
-pact-mock-service start --pact-specification-version 2.0.0 --log "./tmp/pact.log" --pact-dir "./tmp/pacts" -p 1234
-
+#!/bin/sh
+mkdir -p "${SRCROOT}/tmp"
+pact-mock-service start --pact-specification-version 2.0.0 --log "${SRCROOT}/tmp/pact.log" --pact-dir "${SRCROOT}/tmp/pacts" -p 1234
 swift build
-
 swift test
-
 pact-mock-service stop
