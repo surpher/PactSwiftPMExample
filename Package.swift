2f4e69c69338e620bac4dcc3c1ccef476aebf758
Marko Justinek <marko.justinek@ittybittyapps.com>
2019-06-27 08:09:57 +1000

    Update dependencies’ versions

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "PactSwiftPMExample",
     dependencies: [
-      .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.5.1"),
-      .package(url: "https://github.com/DiUS/pact-consumer-swift", from: "0.6.4")
+      .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.2"),
+      .package(url: "https://github.com/DiUS/pact-consumer-swift", from: "0.6.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
